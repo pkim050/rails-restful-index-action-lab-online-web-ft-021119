67c22b11e674f480fd2b3fc7c0afc7c7d6479b4f
Patrick Kim <pkim050@gmail.com>
2019-04-20 12:34:16 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,9 +1,10 @@
 class StudentController < ApplicationController
   get '/students' do
+    @array = Student.all
     erb :index
   end
 
   post '/students' do
-
+    @array = Student.all
   end
 end
